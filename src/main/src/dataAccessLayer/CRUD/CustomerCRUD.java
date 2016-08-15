bc9cee5131ba2841f3c93012f717da0048512a8c
Marzie <marziepoorkashefi@gmail.com>
2016-08-15 10:42:26 +0430

    backup

--- src/main/src/dataAccessLayer/CRUD/CustomerCRUD.java
+++ src/main/src/dataAccessLayer/CRUD/CustomerCRUD.java
@@ -1,13 +1,14 @@
 package dataAccessLayer.CRUD;
 
 import dataAccessLayer.Customer;
+import exception.DatabaseException;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-import static dataAccessLayer.DBConnection.getDBConnection;
+import static util.DBConnection.getDBConnection;
 
 /**
  * Created by Dotin school 6 on 8/7/2016.
@@ -15,47 +16,51 @@ import static dataAccessLayer.DBConnection.getDBConnection;
 public class CustomerCRUD {
     static PreparedStatement preparedStatement = null;
 
-    public static Customer createCustomer() {
+    public static Customer createCustomer() throws DatabaseException {
         long id = 01;
         Customer customer = new Customer();
         String customerNumber = generateCustomerNumber();
         customer.setCustomerNumber(customerNumber);
         //TODO INSERT INTO CUSTOMER TABLE
         try {
-            preparedStatement = getDBConnection().prepareStatement("INSERT INTO customer(customer_number) VALUES (?)" , Statement.RETURN_GENERATED_KEYS);
-            preparedStatement.setString(1,customerNumber);
+            preparedStatement = getDBConnection().prepareStatement("INSERT INTO customer(customer_number) VALUES (?)", Statement.RETURN_GENERATED_KEYS);
+            preparedStatement.setString(1, customerNumber);
             preparedStatement.executeUpdate();
             ResultSet resultSet = preparedStatement.getGeneratedKeys();
-            if (resultSet.next()){
-              id = (long) resultSet.getInt(1);
+            if (resultSet.next()) {
+                id = (long) resultSet.getInt(1);
             }
             customer.setId(id);
             resultSet.close();
         } catch (SQLException e) {
-            e.printStackTrace();
+            throw new DatabaseException("خطا در پایگاه داده...");
         }
         return customer;
     }
 
-    public static String generateCustomerNumber() {
-        String customerNumber = null;
+    public static String generateCustomerNumber() throws DatabaseException {
+        String customerNumber = "";
         try {
             preparedStatement = getDBConnection().prepareStatement("SELECT MAX(customer_number) FROM customer");
 
             ResultSet resultSet = preparedStatement.executeQuery();
-            boolean result = resultSet.next();
-            if (result) {
+            resultSet.next();
+            if (resultSet.getInt(1) != 0) {
                 customerNumber = String.valueOf(resultSet.getInt(1) + 1);
-
             } else {
+                customerNumber = "100";
                 //legalCustomer.setCustomerNumber(1000);
-                customerNumber = "1000";
             }
 
         } catch (SQLException e) {
-            e.printStackTrace();
+            throw new DatabaseException("خطا در پایگاه داده...");
         }
         return customerNumber;
     }
 
+    public static void DeleteCustomer(long id) throws SQLException {
+        preparedStatement = getDBConnection().prepareStatement("DELETE FROM customer WHERE id=?");
+        preparedStatement.setLong(1, id);
+        preparedStatement.executeUpdate();
+    }
 }
